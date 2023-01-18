2cacc1710d83efe6cf62175f204a3c2dd9d45c84
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-01-18 22:24:17 +0530

    Pushed API

---
+++ main.py
@@ -0,0 +1,25 @@
+import pyfiglet
+import uvicorn
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+
+@app.get("/")
+def main(text: str, font: str = "standard"):
+    if font not in pyfiglet.FigletFont.getFonts():
+        return HTTPException(
+            status_code=404,
+            detail=f"Font '{font}' not found. Check /fonts for available fonts.",
+        )
+    res = pyfiglet.figlet_format(text, font=font)
+    return {"status_code": 200, "result": res}
+
+
+@app.get("/fonts/")
+def fonts():
+    return {"status_code": 200, "fonts": pyfiglet.FigletFont.getFonts()}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)

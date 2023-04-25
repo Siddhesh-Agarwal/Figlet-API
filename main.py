bb3c50b1e82cd4ad4b4a0375f22f210f64db8a95
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-04-25 12:44:15 +0530

    error handler

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+# convert text to ascii art
 @app.route("/")
 def main():
     text = request.args.get("text")
@@ -32,9 +33,21 @@ def main():
     return jsonify({"text": text, "font": font, "ascii": res})
 
 
+# list all available fonts
 @app.route("/fonts")
 def fonts():
     return pyfiglet.FigletFont.getFonts()
+
+
+# error handler
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify({"status_code": 400, "detail": "Bad request"}), 400
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"status_code": 404, "detail": "Not found"}), 404
 
 
 if __name__ == "__main__":
